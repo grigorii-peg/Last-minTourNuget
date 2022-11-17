1fda9a7a7b5eb95e145e696c46ad13afff2ae5b0
grigorii-peg <106974779+grigorii-peg@users.noreply.github.com>
2022-11-17 20:37:31 +0300

    Add files via upload

---
+++ Last-min TourNu/Tours.cs
@@ -0,0 +1,26 @@
+﻿namespace Last_min_TourNu
+{
+    public class Tours<T> where T : class
+    {
+        private List<T> tours = new List<T>();
+
+        public Tours() { }
+        public List<T> GetList()
+        {
+            return tours;
+        }
+        public void Add(T arg)
+        {
+            tours.Add(arg);
+        }
+        public void Edit(T id, T arg)
+        {
+            var index = tours.IndexOf(id);
+            tours[index] = arg;
+        }
+        public void Delete(T arg)
+        {
+            tours.Remove(arg);
+        }
+    }
+}
